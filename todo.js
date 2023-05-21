101daf6126a24d6fd31ddae564cd38de532fe884
Gohil Mitul Bhavesh <119096897+gohilmitulb@users.noreply.github.com>
2023-05-21 12:17:09 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,29 +1,46 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
+document.querySelector('#push').onclick = function () {
+    var newTaskInput = document.querySelector('#newtask input');
+    var taskName = newTaskInput.value;
+
+    if (taskName.length == 0) {
         alert("Please enter a task");
-    }
-    else{
-        document.querySelector('#tasks').innerHTML
-        +=`<div class="task">
-        <span id="taskname">
-        ${document.querySelector('#newtask input').value}
-    </span>
-    <button class="delete">X</button>
-    </div>`;
-    var current_tasks = document.querySelectorAll('.delete');
-    for(var i = 0; i < current_tasks.length; i++)
-    {
-        current_tasks[i].onclick = function(){
-            this.parentNode.remove();
+    } else {
+        var pendingTasksContainer = document.querySelector('#pending-tasks');
+        var tasksSmall = pendingTasksContainer.querySelector('small');
+        if (tasksSmall) {
+            tasksSmall.remove();
         }
-    }
-    var tasks = document.querySelectorAll('#taskname');
-    for(var i=0; i<tasks.length; i++)
-    {
-        tasks[i].onclick = function(){
-            this.classList.toggle('completed');
+
+        var taskElement = document.createElement('div');
+        taskElement.classList.add('task');
+        taskElement.innerHTML = `
+            <span id="taskname">${taskName}</span>
+            <button class="complete">&#10003;</button>
+        `;
+        pendingTasksContainer.appendChild(taskElement);
+
+        var completeBtns = document.querySelectorAll('.complete');
+        var completedTasksContainer = document.querySelector('#completed-tasks');
+
+        for (var i = 0; i < completeBtns.length; i++) {
+            completeBtns[i].onclick = function () {
+                var taskElement = this.parentNode;
+                var taskName = taskElement.querySelector('#taskname').textContent;
+
+                var completedTaskElement = document.createElement('div');
+                completedTaskElement.classList.add('task');
+                completedTaskElement.innerHTML = `<span id="taskname" style="text-decoration: line-through;">${taskName}</span>`;
+                completedTasksContainer.appendChild(completedTaskElement);
+                taskElement.remove();
+
+                var removeSmallCompleted = document.querySelector('#completed-tasks');
+                var tasksSmall = removeSmallCompleted.querySelector('small');
+                if (tasksSmall) {
+                    tasksSmall.remove();
+                }
+            }
         }
+
+        newTaskInput.value = "";
     }
-    document.querySelector('#newtask input').value = "";
-    }   
 }
